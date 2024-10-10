2d7f37123634e0d1305e84b167e8a729aeabb695
Maryama <you@example.com>
2024-10-10 17:32:32 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// trim is cutted the space of the praragraph
+// spilt changes as arrey
+document.getElementById("text-input").addEventListener("input", function(event){
+    const inputFieldValue = event.target.value;
+
+    const wordCount = inputFieldValue.trim().split(/\s+/).filter(function(word){
+        
+        return word.length > 0;
+    }).length
+
+    const charCount = inputFieldValue.length;
+
+    document.getElementById("word-count").textContent = wordCount;
+    
+    document.getElementById("char-count").textContent = charCount;
+
+    const twitterLimit = 200 - charCount;
+
+    const facebookLimit = 63206 - charCount;
+
+    document.getElementById("twitter-limit").textContent = twitterLimit >= 0 ? twitterLimit : 0;
+
+    document.getElementById("facebook").textContent = facebookLimit >= 0 ? facebookLimit : 0;
+
+});
